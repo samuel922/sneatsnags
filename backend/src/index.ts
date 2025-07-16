97f72dced20ed8250e70293e892664ace88de3c5
samuel922 <onyangosamuel583@gmail.com>
2025-07-16 15:24:37 +0300

    updated cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,12 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:3001",
+      "http://localhost:5173",
+      "https://sneatsnags.onrender.com",
+      "https://sneatsnags-frontend.onrender.com"
+    ],
     credentials: true,
   })
 );
